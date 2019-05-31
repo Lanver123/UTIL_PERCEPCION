bf2c63ae76accdf8001ea2be5163ae5a6bda1ba2
Lanver <menoxzhan@gmail.com>
2019-05-31 20:32:52 +0200

    KERNEL_COORDENADAS

---
+++ kernelfuncioncoordenadas.m
@@ -0,0 +1,35 @@
+#!/usr/bin/octave -qf
+
+function[] = kernelperceptron(trset,clases)
+
+  if (nargin!=2)
+    printf("Usage: kernelperceptron.m(trset, clases)\n");
+    printf("Donde trset contiene una muestra (x,y) en cada fila");
+    return;
+  endif
+  
+  trset
+  
+  # Cálculo de Gramm
+  gramm = zeros(rows(trset), rows(trset));
+  for i=1:rows(trset)
+    for j=1:(rows(trset)-i+1)
+      # Distancia de Hamming, solo si la piden
+      igu = (trset(i,:) == trset(j,:));
+      dxy = sum(ones(1,2) - igu);
+      # Valor para K = 1 / (dxy + 1)
+      gramm(i,j) = 1/(dxy+1);
+      
+      # Valor para K = exp(x'y)
+      # gramm(i,j) = e^(trset(j,:)*trset(i,:)');
+      
+      # Valor para K = ???
+      # examen
+      
+      gramm(j,i) = gramm(i,j);
+    endfor
+  endfor
+  
+  kernelperceptron(gramm,clases,1)
+  
+endfunction
---
+++ kernelperceptron.m
@@ -0,0 +1,47 @@
+#!/usr/bin/octave -qf
+
+function[] = kernelperceptron(gramm,clases,iteraciones)
+
+  if (nargin!=3)
+    printf("Usage: kernelperceptron.m(gramm, clases, iteraciones)\n");
+    return;
+  endif
+  
+  gramm
+  clases
+  
+  disp("Pesos iniciales:");
+  alphas = zeros(1, rows(gramm));
+  disp(alphas);
+  printf("\n");
+  
+  #g(x)=sum_1_n(alphai*ci*K(xi,x)+alphai*ci)
+  
+  for iter=1:iteraciones
+    printf("----------------------------------\nIteración %i\n\n", iter);
+  
+    for i=1:rows(gramm)
+      aux = 0;
+      for j=1:rows(gramm)
+        if (i != j)
+          val1 = alphas(1,j)*clases(1,j)*gramm(i,j);
+          val2 = alphas(1,j)*clases(1,j);
+          if (val1 != 0 && val2 != 0)
+             printf("a%i*c%i*K(x%i,x%i) + a%i*c%i = %d + %d\n",j,j,j,i,j,j,val1,val2);
+          endif
+          aux = aux + val1 + val2;
+        endif
+      endfor
+      if (clases(1,i) * aux <= 0)
+        alphas(1,i) = alphas(1,i) + 1;
+      endif
+      printf("Valor de g(x): %d\n",aux);
+      printf("Valor de cig(x): %d\n",clases(1,i) * aux);
+      printf("Pesos tras analizar x%i\n",i);
+      disp(alphas);
+      printf("\n");
+    endfor
+   
+  endfor
+  
+endfunction
